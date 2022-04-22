06c00c5d69ec849ff6207097d9a542258b809749
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-04-22 17:30:30 +0430

    Cake Component

---
+++ src/components/CakeCom.js
@@ -0,0 +1,10 @@
+const CakeCom = () => {
+  return (
+    <section className="container">
+      <h2>amount cake -</h2>
+      <button>by cake</button>
+    </section>
+  );
+};
+
+export default CakeCom;
