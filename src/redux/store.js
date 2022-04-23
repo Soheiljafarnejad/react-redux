7c2e0e38b66ef4e963412c10a0256155fe52c4be
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-04-23 12:52:43 +0430

    redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,23 @@ import { iceReducer } from "./iceReducer";
 import { asyncReducer } from "./asyncReducer";
 import thunk from "redux-thunk";
 
+// saga
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas/rootSaga";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
 const reducer = combineReducers({
   cake: cakeReducer,
   ice: iceReducer,
   user: asyncReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(
+  reducer,
+  applyMiddleware(thunk, sagaMiddleware)
+);
+
+// then run the saga
+sagaMiddleware.run(rootSaga);
